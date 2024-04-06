39458300991044ed2f3d2fb6f9812b9d36cc5afa
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-04-07 02:55:17 +0500

    github

---
+++ index.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let currency:any = {
+    USD : 1 , // base currency
+    EUR:  0.91,
+    IND:  83.30,
+    GBP:   8.76,
+    CNY:  7.23,
+    PKR: 280,
+};
+let user_Amount = await inquirer.prompt(
+    [
+        {
+            name:"from",
+            message:"Enter from Currency",
+            type:"list",
+            choices:["USD","EUR","IND","GBP","CNY","PKR"]
+        },
+        {
+            name:"to",
+            message:"Enter to Currency",
+            type:"list",
+            choices:["USD","EUR","IND","GBP","CNY","PKR"]
+        },
+        {
+            name:"amount",
+            message:"Enter Your amount",
+            type:"number"
+        }
+    ]
+)
+let fromAmount = currency[user_Amount.from]   // exchange rate
+let toAmount = currency[user_Amount.to]       // exchange rate
+let amount = user_Amount.amount
+let baseAmount = amount/fromAmount
+let convertedAmount = baseAmount*toAmount   // USD base currency
+console.log(convertedAmount);
